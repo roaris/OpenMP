7b4632a92e93c0b2ee347fd77368d0285a374167
roaris <one0803kyopro@gmail.com>
2021-06-09 02:34:26 +0900

    Update matrixMultiplication.f90

--- matrixMultiplication.f90
+++ matrixMultiplication.f90
@@ -6,21 +6,21 @@ integer :: i, j, k
 real(kind=8) :: rand
 
 !$omp parallel do
-    do j = 1, N
-        do i = 1, N
-            a(i, j) = rand(0)
-            b(i, j) = rand(0)
-            c(i, j) = 0.0D0
-        end do
+do j = 1, N
+    do i = 1, N
+        a(i, j) = rand(0)
+        b(i, j) = rand(0)
+        c(i, j) = 0.0D0
     end do
+end do
 
 !$omp parallel do
-    do j = 1, N
-        do k = 1, N
-            do i = 1, N
-                c(i, j) = c(i, j) + a(i, k) * b(k, j)
-            end do
+do j = 1, N
+    do k = 1, N
+        do i = 1, N
+            c(i, j) = c(i, j) + a(i, k) * b(k, j)
         end do
     end do
+end do
 
 end program matrixMultiplication
